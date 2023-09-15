1757bfb1ee35f3843ca10c316ca2fbbd927b149e
noriyotcp <noriyo.akita@gmail.com>
2023-09-16 04:33:48 +0900

    List users by the table of Shadcn
    npx shadcn-ui@latest add button

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,32 +1,49 @@
-import RootLayout from "../layout";
 import { users } from "@/lib/users";
 import Link from "next/link";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
 
 export default function Users() {
-  const [firstUser, ...rest] = users;
-
   return (
     <>
       <h2 className="text-xl mb-5">Welcome back, Users!</h2>
       <p>Here is the overview of users:</p>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5">
-        <Link href={`users/${firstUser.id.toString()}`}>
-          <div className="p-5 border rounded-md">
-            <h3 className="text-lg mb-2">{firstUser.name}</h3>
-            <p>@{firstUser.username}</p>
-            <p>{firstUser.email}</p>
-          </div>
-        </Link>
-        {rest.map((user) => (
-          <Link href={`users/${user.id.toString()}`} key={user.id}>
-            <div className="p-5 border rounded-md" key={user.id}>
-              <h3 className="text-lg mb-2">{user.name}</h3>
-              <p>@{user.username}</p>
-              <p>{user.email}</p>
-            </div>
-          </Link>
-        ))}
-      </div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">ID</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Username</TableHead>
+            <TableHead>Email</TableHead>
+            <TableHead></TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {users.map((user) => {
+            return (
+              <TableRow key={user.id}>
+                <TableCell>{user.id}</TableCell>
+                <TableCell>{user.name}</TableCell>
+                <TableCell>{user.username}</TableCell>
+                <TableCell>{user.email}</TableCell>
+
+                <TableCell>
+                  <Link href={`users/${user.id}`}>
+                    <Button variant="link">Link</Button>
+                  </Link>
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
     </>
   );
 }
