b47a5ac994ad32432dc06f59afe9e85eb88a11d3
noriyotcp <noriyo.akita@gmail.com>
2023-09-16 13:38:26 +0900

    Fetch user via api/users/[id]
    Not found via API response

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,21 +1,27 @@
-import { users } from '@/lib/users';
 import { notFound } from "next/navigation";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
-const fetchUsers = (id: string) => {
-  return users.find((user) => user.id === parseInt(id));
-}
+const fetchUser = async (id: string) => {
+  const res = await fetch(`http://localhost:3000/api/users/${id}`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  return res;
+};
 
-export default function User({ params }: { params: { id: string } }) {
-  const user = fetchUsers(params.id);
-  if (user === undefined) {
+
+export default async function User({ params }: { params: { id: string } }) {
+  const res = await fetchUser(params.id);
+  if (!res.ok) {
     return notFound();
   }
+  const user = await res.json();
 
   return (
     <>
-      <h2 className="text-xl mb-5">Welcome back, {user?.name}!</h2>
+      <h2 className="text-xl mb-5">Welcome back, {user.name}!</h2>
       <Button variant="link">
         <Link href="/users">Back to Users</Link>
       </Button>
