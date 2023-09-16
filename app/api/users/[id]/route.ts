e9c10424cd807e268ada51933867e58667bc77c7
noriyotcp <noriyo.akita@gmail.com>
2023-09-16 13:14:47 +0900

    Assign params via destructuring from context

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -4,8 +4,8 @@ const fetchUser = (id: string) => {
   return users.find((user) => user.id === parseInt(id));
 };
 
-export async function GET(request: Request, context: any) {
-  const user = fetchUser(context.params.id);
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+  const user = fetchUser(params.id);
   if (!user) {
     return NextResponse.json({ message: "User not found" }, { status: 404 });
   }
