fa362fc96fed0dfee09560b016072bff8492600a
noriyotcp <noriyo.akita@gmail.com>
2023-09-16 20:57:16 +0900

    Fetch user via prisma client

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,13 +1,20 @@
 import { NextResponse } from "next/server";
 import { users } from "@/lib/users";
-const fetchUser = (id: string) => {
-  return users.find((user) => user.id === parseInt(id));
+import prisma from "@/lib/prisma";
+import { Prisma } from "@prisma/client";
+
+const fetchUser = async (id: string) => {
+  try {
+    const user = await prisma.user.findUniqueOrThrow({ where: { id: id } });
+    return NextResponse.json(user, { status: 200 });
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
+  }
 };
 
 export async function GET(request: Request, { params }: { params: { id: string } }) {
-  const user = fetchUser(params.id);
-  if (!user) {
-    return NextResponse.json({ message: "User not found" }, { status: 404 });
-  }
-  return NextResponse.json(user, { status: 200 });
+  const res = await fetchUser(params.id);
+  return res;
 }
