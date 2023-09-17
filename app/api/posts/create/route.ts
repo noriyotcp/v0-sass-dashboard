d52f532cd4a3c60388b1a1179820e5801bd95716
noriyotcp <noriyo.akita@gmail.com>
2023-09-18 03:36:12 +0900

    Create a new post via Prisma

--- app/api/posts/create/route.ts
+++ app/api/posts/create/route.ts
@@ -3,5 +3,14 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
   const data = await request.json();
   const { title, content } = data;
-  return NextResponse.json({ title, content }, { status: 201 });
+
+  const result = await prisma.post.create({
+    data: {
+      title,
+      content,
+      published: false,
+    },
+  });
+
+  return NextResponse.json(result, { status: 201 });
 }
