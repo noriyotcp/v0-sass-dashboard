f0e9739d5e861ab495133a82af4642bfaae3252c
noriyotcp <noriyo.akita@gmail.com>
2023-09-18 03:24:01 +0900

    Clean up and format posts/create/page.tsx

--- app/posts/create/page.tsx
+++ app/posts/create/page.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-import React, { useState } from "react";
 import { PostForm } from "./form";
 
-const Draft: React.FC = () => {
+export default function Draft() {
   return (
     <>
       <PostForm />
     </>
   );
-};
-
-export default Draft;
+}
