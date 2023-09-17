5da44cddc20e8148ac1ceb718b95e2123b3add57
noriyotcp <noriyo.akita@gmail.com>
2023-09-17 15:57:53 +0900

    Update seed.ts for posts and format document

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,12 @@
-import { PrismaClient, Prisma } from '@prisma/client'
+import { PrismaClient, Prisma } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-type UserParams = Pick<Prisma.UserCreateInput, 'name' | 'username' | 'email'>
+type UserParams = Pick<Prisma.UserCreateInput, "name" | "username" | "email">;
+type PostParams = Pick<
+  Prisma.PostCreateInput,
+  "id" | "title" | "content" | "published"
+>;
 
 const users: UserParams[] = [
   {
@@ -22,6 +26,26 @@ const users: UserParams[] = [
   },
 ];
 
+const posts: PostParams[] = [
+  {
+    title:
+      "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    content:
+      "quia et suscipit↵suscipit recusandae consequuntur expedita et cum↵reprehenderit molestiae ut ut quas totam↵nostrum rerum est autem sunt rem eveniet architecto",
+    published: true,
+  },
+  {
+    title: "qui est esse",
+    content:
+      "est rerum tempore vitae↵sequi sint nihil reprehenderit dolor beatae ea dolores neque↵fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis↵qui aperiam non debitis possimus qui neque nisi nulla",
+    published: true,
+  },
+  {
+    title: "This is a draft",
+    published: false,
+  },
+];
+
 async function main() {
   users.forEach(async (user) => {
     await prisma.user.upsert({
@@ -32,17 +56,30 @@ async function main() {
         username: user.username,
         email: user.email,
       },
-    })
-    console.log({ user })
-  })
+    });
+    console.log({ user });
+  });
+
+  posts.forEach(async (post) => {
+    await prisma.post.upsert({
+      where: { id: post.id },
+      update: {},
+      create: {
+        title: post.title,
+        content: post.content,
+        published: post.published,
+      },
+    });
+    console.log({ post });
+  });
 }
 
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
