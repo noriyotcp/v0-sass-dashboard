76a561f4a0a704dfd3101d1c0847bc70d48907a6
noriyotcp <noriyo.akita@gmail.com>
2023-09-18 20:50:49 +0900

    Add EditPostForm to app/posts/form.tsx

--- app/posts/form.tsx
+++ app/posts/form.tsx
@@ -112,3 +112,87 @@ export function NewPostForm() {
     }
   }
 }
+
+export function EditPostForm() {
+  const router = useRouter();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      content: "",
+    },
+  });
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input placeholder="Title..." {...field} autoFocus />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Content</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Content..."
+                  className="resize-none"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" disabled={isSubmitting}>
+          Submit
+        </Button>
+        <Button variant="outline" className="ml-2">
+          <a className="back" href="#" onClick={() => router.replace("/posts")}>
+            or Cancel
+          </a>
+        </Button>
+      </form>
+    </Form>
+  );
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    setIsSubmitting(true);
+
+    try {
+      const body = { title: values.title, content: values.content };
+      await fetch("/api/posts/create", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      }).then((res) => {
+        if (res.status === 201) {
+          console.log("redirecting");
+          router.push("/posts?published=false");
+          startTransition(() => {
+            router.refresh();
+          });
+        }
+      });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsSubmitting(false);
+      console.log(values);
+    }
+  }
+}
