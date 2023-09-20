a4331b68ff293a6bfc2b166e9034f51508a1af97
noriyotcp <noriyo.akita@gmail.com>
2023-09-20 23:59:12 +0900

    Add CSS and use remark-gfm

--- app/posts/[id]/PostContent.tsx
+++ app/posts/[id]/PostContent.tsx
@@ -3,11 +3,12 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import { Post } from "@prisma/client";
+import remarkGfm from "remark-gfm";
 
 export default function PostContent({ post }: { post: Post }) {
   return (
     // <p className="text-2xl dark:text-zinc-400">
-      <ReactMarkdown className="text-2xl post-content" linkTarget={"_blank"}>
+      <ReactMarkdown remarkPlugins={[remarkGfm]} className="markdown" linkTarget={"_blank"}>
         {post.content ? post.content : ""}
       </ReactMarkdown>
     // </p>
